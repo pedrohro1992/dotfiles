f7d4d51c4b630869d05d89d560e6563f1e9e829e
Pedro Oliveira <pedro_hro1992@outlook.com>
2025-07-15 10:30:46 -0300

    Adiciona nova configuracao wezterm

--- WezTerm/wezterm.lua
+++ WezTerm/wezterm.lua
@@ -1,30 +1,38 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-local mux = wezterm.mux
 
 -- Always start WezTerm maximized
+local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
 
-local mux = wezterm.mux
-
 -- This table will hold the configuration.
 local config = {}
-
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
 -- ###########################################
 -- General congis:
-
--- Set the default domain to WSL Ubuntu
-config.default_domain = "WSL:Ubuntu"
-
--- Set the GPU Driver for the terminal
-config.front_end = "WebGpu"
+-- Performance settings
+-- Configure GPU Render
+config.front_end = "OpenGL"
+-- config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
+config.max_fps = 120
+config.animation_fps = 1
+-- config.window_background_opacity = 0.95
+config.enable_scroll_bar = false
+config.use_fancy_tab_bar = true
+config.term = "xterm-256color"
+config.warn_about_missing_glyphs = false
+config.enable_wayland = false
+config.webgpu_power_preference = "HighPerformance"
+config.prefer_egl = true
+config.freetype_load_target = "Light"
+-- config.freetype_render_target = "HorizontalLcd"
 
 -- Configure leader
 -- timeout_milliseconds defaults to 1000 and can be omitted
@@ -37,46 +45,12 @@ config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
-config.status_update_interval = 1000
-wezterm.on("update-status", function(window, pane)
-	-- Workspace name
-	local stat = window:active_workspace()
-	local stat_color = "#f7768e"
-	-- It's a little silly to have workspace name all the time
-	-- Utilize this to display LDR or current key table name
-	if window:active_key_table() then
-		stat = window:active_key_table()
-		stat_color = "#7dcfff"
-	end
-	if window:leader_is_active() then
-		stat = "LDR"
-		stat_color = "#bb9af7"
-	end
 
-	local basename = function(s)
-		-- Nothing a little regex can't fix
-		return string.gsub(s, "(.*[/\\])(.*)", "%2")
-	end
-
-	-- Left status (left of the tab line)
-	window:set_right_status(wezterm.format({
-		{ Foreground = { Color = stat_color } },
-		{ Text = "|  " },
-		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-		{ Text = " |" },
-	}))
-
-	-- Right status
-end)
 -- Theme
 config.color_scheme = "Catppuccin Mocha (Gogh)"
-
 -- Font Conf
 config.font = wezterm.font("CaskaydiaMono Nerd Font")
 config.font_size = 13
-
--- Desable the defaults keymaps
-config.disable_default_key_bindings = true
 
 -- ###########################################
 
@@ -161,42 +135,10 @@ config.keys = {
 	-- Create and find Workspaces
 	-- Switch to the default workspace
 	{
-		key = "y",
-		mods = "CTRL|SHIFT",
+		key = "d",
+		mods = "LEADER",
 		action = wezterm.action.SwitchToWorkspace({
 			name = "default",
-		}),
-	},
-	-- Prompt for a name to use for a new workspace and switch to it.
-	{
-		key = "W",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.PromptInputLine({
-			description = wezterm.format({
-				{ Attribute = { Intensity = "Bold" } },
-				{ Foreground = { AnsiColor = "Fuchsia" } },
-				{ Text = "Enter name for new workspace" },
-			}),
-			action = wezterm.action_callback(function(window, pane, line)
-				-- line will be `nil` if they hit escape without entering anything
-				-- An empty string if they just hit enter
-				-- Or the actual line of text they wrote
-				if line then
-					window:perform_action(
-						wezterm.action.SwitchToWorkspace({
-							name = line,
-						}),
-						pane
-					)
-				end
-			end),
-		}),
-	},
-	{
-		key = "F",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.ShowLauncherArgs({
-			flags = "FUZZY|WORKSPACES",
 		}),
 	},
 
@@ -220,29 +162,24 @@ config.keys = {
 	},
 }
 
--- Plugins
-local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
-bar.apply_to_config(config)
-bar.apply_to_config(config, {
-	position = "top",
-	modules = {
-		username = {
-			enabled = false,
-		},
-		hostname = {
-			enabled = false,
-		},
-		clock = {
-			enabled = false,
-		},
-		cwd = {
-			enabled = false,
-		},
+-- Desable the defaults keymaps
+config.disable_default_key_bindings = false
+
+-- ###########################################
+
+-- Plugins:
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup()
+
+local wpr = wezterm.plugin.require("https://github.com/vieitesss/workspacesionizer.wezterm")
+wpr.apply_to_config(config, {
+	paths = { "~/Workspace/github.com/*", "~/Workspace/misc.local/*" },
+	git_repos = false,
+	show = "full",
+	binding = {
+		key = "o",
+		mods = "LEADER",
 	},
 })
 
-local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
-workspace_switcher.apply_to_config(config)
-
--- return the config to wezterm
 return config
